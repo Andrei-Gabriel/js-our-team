db63e12a30f530fd91c2fa88712fcb5aac00541f
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-11-20 14:37:04 +0100

    Aggiunto elementi all'oggetto e stampa

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-alert("ciao");
+// Creazione array di oggetti che rappresentano ciascun membro del team
+const members = [
+    {
+        "name": "Wayne",
+        "surname": "Barnett",
+        "role": "CEO",
+        "img": "img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        "name": "Walter",
+        "surname": "Gordon",
+        "role": "Office Manager",
+        "img": "img/walter-gordon-office-manager.jpg"
+    },
+    {
+        "name": "Scott",
+        "surname": "Estrada",
+        "role": "Developer",
+        "img": "img/scott-estrada-developer.jpg"
+    },
+    {
+        "name": "Barbara",
+        "surname": "Ramos",
+        "role": "Graphic Designer",
+        "img": "img/barbara-ramos-graphic-designer.jpg"
+    },
+    {
+        "name": "Angela",
+        "surname": "Lopez",
+        "role": "Social Media Manager",
+        "img": "img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "name": "Angela",
+        "surname": "Caroll",
+        "role": "Chief Editor",
+        "img": "img/angela-caroll-chief-editor.jpg"
+    },
+];
+
+// Stampa dell'intero oggetto
+console.log(members);
